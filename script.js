4aef274acc6199fa2499cc315b23e85999e64aa2
Carlos88Pereira <146039244+Carlos88Pereira@users.noreply.github.com>
2024-02-15 20:07:42 +0000

    final

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const app=Vue.createApp(
             return{
                 productName: "Cruise Ship",
                 productDescription: "Cruise Ship Description",
-                productSubtitle: "Subtitle of page"
-
+                productSubtitle: "Subtitle of page",
+                ship: "https://media.istockphoto.com/id/104241367/pt/foto/barco-de-cruzeiro.jpg?s=1024x1024&w=is&k=20&c=QN8uz9IxTVxW5he8hbQYQKgIros565hyC-iXSoedwJM=",
+                substyle: {'background-color': "navy", 'color' : "blue"
+                }
         }
     }
     }
