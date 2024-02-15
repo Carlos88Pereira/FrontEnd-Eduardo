34a182e6630dbcae6ffbca0aa3f33120123482c0
Carlos88Pereira <146039244+Carlos88Pereira@users.noreply.github.com>
2024-02-15 18:54:31 +0000

    excercicio feito

---
+++ script.js
@@ -0,0 +1,14 @@
+const app=Vue.createApp(
+    {
+        data()
+        {
+            return{
+                productName: "Cruise Ship",
+                productDescription: "Cruise Ship Description",
+                productSubtitle: "Subtitle of page"
+
+        }
+    }
+    }
+)
+app.mount("#app")
